a5a529ae4adabedacda4d8095ece68927bfaf6b9
iuxcode <charlottabade@gmail.com>
2024-07-16 17:07:28 +0000

    better flukit interfacing

--- lib/flukit_core.dart
+++ lib/flukit_core.dart
@@ -7,7 +7,10 @@ export 'package:flukit_core/src/data/static/countries.dart';
 /// Share core utilities & functions across all packages of the flukit framework
 abstract class Flukit {}
 
-class _FluImpl implements Flukit {
+class _FluImpl implements Flukit {}
+
+// ignore: public_member_api_docs
+extension FlukitExt on Flukit {
   /// list of the countries of the world with
   /// their information: `name`, `isoCode`, `iso3Code` and the `phoneCode`
   List<Country> get countries => Country.all;
