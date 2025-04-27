d18a92f94bf0fdeaddab9680209e38cb3e137d08
iuxcode <charlottabade@gmail.com>
2025-04-27 07:58:06 +0000

    refactor Flukit class to use a factory constructor for singleton instantiation

--- lib/flukit_core.dart
+++ lib/flukit_core.dart
@@ -6,15 +6,6 @@ export 'package:flukit_core/src/data/static/countries.dart';
 
 /// Share core utilities & functions across all packages of the flukit framework
 class Flukit {
-  /// Flukit constructor
-  factory Flukit() {
-    return _instance;
-  }
-
-  Flukit._internal();
-
-  static final Flukit _instance = Flukit._internal();
-
   /// list of the countries of the world with
   /// their information: `name`, `isoCode`, `iso3Code` and the `phoneCode`
   List<Country> get countries => Country.all;
